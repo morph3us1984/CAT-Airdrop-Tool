dd9c6ddf52f36763d735b1c1e164d8eb31a3a953
morph3us1984 <89521988+morph3us1984@users.noreply.github.com>
2022-03-20 02:08:15 +0100

    Update CATairdrop.sh

--- CATairdrop.sh
+++ CATairdrop.sh
@@ -3,7 +3,7 @@
 #Version 0.1a
 exec 3>&1 4>&2
 trap 'exec 2>&4 1>&3' 0 1 2 3
-exec 1>coinsplit.log 2>&1
+exec 1>cat-airdrop.log 2>&1
 # Everything below will go to the file 'log.out':
 set -o xtrace
 echo "Coinsplit script started:"
